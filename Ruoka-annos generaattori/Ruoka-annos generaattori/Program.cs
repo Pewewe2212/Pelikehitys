0b153a680a7212c5c5b2fa751a41a8e5d0bb34f3
Pewewe2212 <petteri.viitalahde@gmail.com>
2025-01-20 10:33:16 +0200

    Ruoka generaattori

---
+++ Ruoka-annos generaattori/Ruoka-annos generaattori/Program.cs
@@ -0,0 +1,121 @@
+﻿namespace Ruoka_annos_generaattori
+{
+    internal class Program
+    {
+        public class Food
+        {
+            public enum Main { Nautaa, Kanaa, Kasviksia }
+            public enum Side { Perunaa, Riisiä, Pastaa }
+            public enum Sauce { Curry, Pippuri, Hapanimelä, Chili }
+        }
+
+        static void Main(string[] args)
+        {
+            Food food = new Food();
+            string choice;
+            string Main;
+            string Side;
+            string Sauce;
+
+            while (true)
+            {
+                Console.WriteLine("Pääraaka-aine (Nautaa, Kanaa, Kasviksia): ");
+                choice = Console.ReadLine();
+                if (choice.ToLower() == "Nautaa".ToLower())
+                {
+                    Main = "Nautaa";
+                    break;
+                }
+                else if (choice.ToLower() == "Kanaa".ToLower())
+                {
+                    Main = "Kanaa";
+                    break;
+                }
+                else if (choice.ToLower() == "Kasviksia".ToLower())
+                {
+                    Main = "Kasviksia";
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("En tunnistanut valintaasi");
+                }
+            }
+
+            while (true)
+            {
+                Console.WriteLine("Lisukkeet (Perunaa, Riisiä, Pastaa): ");
+                choice = Console.ReadLine();
+                if (choice.ToLower() == "Perunaa".ToLower())
+                {
+                    Side = "Perunaa";
+                    break;
+                }
+                else if (choice.ToLower() == "Riisiä".ToLower())
+                {
+                    Side = "Riisiä";
+                    break;
+                }
+                else if (choice.ToLower() == "Pastaa".ToLower())
+                {
+                    Side = "Pastaa";
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("En tunnistanut valintaasi");
+                }
+            }
+
+            while (true)
+            {
+                Console.WriteLine("Kastike (Curry, Pippuri, Hapanimelä, Chili): ");
+                choice = Console.ReadLine();
+                if (choice.ToLower() == "Curry".ToLower())
+                {
+                    Sauce = "Curry";
+                    break;
+                }
+                else if (choice.ToLower() == "Pippuri".ToLower())
+                {
+                    Sauce = "Pippuri";
+                    break;
+                }
+                else if (choice.ToLower() == "Hapanimelä".ToLower())
+                {
+                    Sauce = "Hapanimelä";
+                    break;
+                }
+                else if (choice.ToLower() == "Chili".ToLower())
+                {
+                    Sauce = "Chili";
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("En tunnistanut valintaasi");
+                }
+            }
+
+            Console.WriteLine($"{Main} ja {Side} {Sauce}-kastikkeella");
+
+            Console.WriteLine("Custom order");
+
+            List<string> list = new List<string>();
+
+            Console.WriteLine("Pääraaka-aine: ");
+            choice = Console.ReadLine();
+            list.Add(choice);
+
+            Console.WriteLine("Sivukkeet: ");
+            choice = Console.ReadLine();
+            list.Add(choice);
+
+            Console.WriteLine("Kastike: ");
+            choice = Console.ReadLine();
+            list.Add(choice);
+
+            Console.WriteLine($"{list[0]} ja {list[1]} {list[2]}-kastikkeella");
+        }
+    }
+}
