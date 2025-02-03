8cd72012271b5d46b5a8336621048030fadb5546
Pewewe2212 <petteri.viitalahde@gmail.com>
2025-02-03 10:38:23 +0200

    Robotti

---
+++ Robotti/Program.cs
@@ -0,0 +1,142 @@
+﻿namespace Robotti
+{
+    internal class Program
+    {
+        public abstract class RobottiKäsky
+        {
+            public abstract void Suorita(Robotti robotti);
+        }
+
+        public class Käynnistä : RobottiKäsky
+        {
+            public override void Suorita(Robotti robotti)
+            {
+                robotti.OnKäynnissä = true;
+            }
+        }
+
+        public class Sammuta : RobottiKäsky
+        {
+            public override void Suorita(Robotti robotti)
+            {
+                robotti.OnKäynnissä = false;
+            }
+        }
+
+        public class YlösKäsky : RobottiKäsky
+        {
+            public override void Suorita(Robotti robotti)
+            {
+                if (robotti.OnKäynnissä == true)
+                {
+                    robotti.Y += 1;
+
+                }
+            }
+        }
+        public class AlasKäsky : RobottiKäsky
+        {
+            public override void Suorita(Robotti robotti)
+            {
+                if (robotti.OnKäynnissä == true)
+                {
+                    robotti.Y -= 1;
+
+                }
+            }
+        }
+        public class VasenKäsky : RobottiKäsky
+        {
+            public override void Suorita(Robotti robotti)
+            {
+                if (robotti.OnKäynnissä == true)
+                {
+                    robotti.X -= 1;
+
+                }
+            }
+        }
+        public class OikeaKäsky : RobottiKäsky
+        {
+            public override void Suorita(Robotti robotti)
+            {
+                if (robotti.OnKäynnissä == true)
+                {
+                    robotti.X += 1;
+
+                }
+            }
+        }
+
+        public class Robotti
+        {
+            public int X { get; set; }
+            public int Y { get; set; }
+            public bool OnKäynnissä { get; set; }
+            public RobottiKäsky?[] Käskyt { get; } = new RobottiKäsky?[3];
+
+            public void Suorita()
+            {
+                foreach (RobottiKäsky? käsky in Käskyt)
+                {
+                    käsky?.Suorita(this);
+                    Console.WriteLine($"[{X} {Y} {OnKäynnissä}]");
+                }
+            }
+        }
+
+
+
+
+        static void Main(string[] args)
+        {
+            Robotti robotti = new Robotti();
+
+            for (int i = 0; i < 3; i++)
+            {
+                Console.WriteLine("Anna komento robotille. Vaihtoehdot: Käynnistä, Sammuta, Ylös, Alas, Oikea, Vasen");
+
+                string answer = Console.ReadLine();
+
+                switch (answer.ToLower())
+                {
+                    case "käynnistä":
+                        {
+                            robotti.Käskyt[i] = new Käynnistä();
+                            break;
+                        }
+                    case "sammuta":
+                        {
+                            robotti.Käskyt[i] = new Sammuta();
+                            break;
+                        }
+                    case "ylös":
+                        {
+                            robotti.Käskyt[i] = new YlösKäsky();
+                            break;
+                        }
+                    case "alas":
+                        {
+                            robotti.Käskyt[i] = new AlasKäsky();
+                            break;
+                        }
+                    case "vasen":
+                        {
+                            robotti.Käskyt[i] = new VasenKäsky();
+                            break;
+                        }
+                    case "oikea":
+                        {
+                            robotti.Käskyt[i] = new OikeaKäsky();
+                            break;
+                        }
+                    _:
+                        {
+                            break;
+                        }
+                }
+            }
+            robotti.Suorita();
+        }
+    }
+}
